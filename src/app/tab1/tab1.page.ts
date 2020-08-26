99aafb071e8562b2bf8e617d3bea97de409d014f
Brian Hung <26hungb@hotmail.com.au>
2020-08-26 16:55:50 +1000

    ionic post test

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HTTP } from '@ionic-native/http/ngx';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor(public http: HTTP) {
+  }
+  ngOnInit(){}
 
+  sendPostRequest() {
+    let postData = {
+            "name": "ionicposttestuser",
+            "pass": "customer004@email.com"
+    }
+
+    this.http.post("http://localhost:3000/users", postData, {})
+      .then(data => {
+        console.log(data['_body']);
+       }, error => {
+        console.log(error);
+      });
+  }
 }
