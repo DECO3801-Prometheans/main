e8a301b06976204e270bddbe6609edf5182a0826
ba11breaker <15212219417@163.com>
2020-08-28 16:41:40 +1000

    add httpClient in tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HTTP } from '@ionic-native/http/ngx';
+import { HttpClient } from '@angular/common/http';
 import { FormGroup, FormBuilder } from "@angular/forms";
 import * as jwt from 'jsonwebtoken';
 
@@ -14,7 +15,9 @@ export class Tab2Page implements OnInit {
 
   constructor(
     public http: HTTP,
-    public fb: FormBuilder) { 
+    public fb: FormBuilder,
+    public _http: HttpClient,
+    ) { 
       this.userForm = this.fb.group({
         name: [''],
         password: ['']
@@ -26,10 +29,20 @@ export class Tab2Page implements OnInit {
   login() {
     this.http.post("http://localhost:3000/users/login", this.userForm.value, {})
       .then(data => {
+        console.log(data);
         localStorage.setItem('token', (<any>data)._body);
         console.log((<any>data)._body);
        }, error => {
         console.log(error);
+      });
+  }
+  _login() {
+    this._http.post("http://localhost:3000/users/login", {
+      name: 'testUser',
+      password: 'password',
+    }).
+      subscribe(done => {
+        console.log(done);
       });
   }
 }
