2825283d98aa8f3c1d8d7ebc16798388a9b43100
ba11breaker <15212219417@163.com>
2020-09-04 23:34:19 +1000

    add best sellers

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,6 +13,25 @@ export class HomePage implements OnInit {
     '../../assets/imgs/types-of-fuit-hero-e1559313183734.jpg'
   ]
 
+  public best_sellers = [
+    [
+      {img: '../../assets/imgs/best-seller/brocli.PNG', 
+        brief: 'hfie hofeahwoi oifewhiofe ', price: 14.6}, 
+      {img: '../../assets/imgs/best-seller/carrot.PNG', 
+      brief: 'fdjwof fdowif jiowe fiowifew', price: 14.6}, 
+      {img: '../../assets/imgs/best-seller/corn.PNG', 
+      brief: '2e iwehfo iofwe fiowjewie wof', price: 14.6}, 
+    ],
+    [
+      {img: '../../assets/imgs/best-seller/onion.PNG', 
+        brief: 'fshdi ofsdihfios odosfi dso', price: 14.6}, 
+      {img: '../../assets/imgs/best-seller/potato.PNG', 
+        brief: 'jido iofdsj foisdj oisjdif', price: 14.6}, 
+      {img: '../../assets/imgs/best-seller/spanish.PNG', 
+        brief: 'hufids oifsdjiof oisdjfoi', price: 14.6}, 
+    ],
+  ]
+
   constructor() { }
 
   ngOnInit() {
