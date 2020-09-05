96cbc27a125f5316416b2a1bbd427e48180d0df8
yashik.anand99 <yashik.anand99@gmail.com>
2020-09-05 16:37:41 +1000

    Modified login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,30 @@ const routes: Routes = [
   },  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'forget',
+    loadChildren: () => import('./forget/forget.module').then( m => m.ForgetPageModule)
+  },
+  {
+    path: 'cover',
+    loadChildren: () => import('./cover/cover.module').then( m => m.CoverPageModule)
+  },
+  {
+    path: 'front',
+    loadChildren: () => import('./front/front.module').then( m => m.FrontPageModule)
+  },
+  {
+    path: 'register-farmer',
+    loadChildren: () => import('./register-farmer/register-farmer.module').then( m => m.RegisterFarmerPageModule)
+  },
+  {
+    path: 'register-user',
+    loadChildren: () => import('./register-user/register-user.module').then( m => m.RegisterUserPageModule)
+  },
+  {
+    path: 'verification',
+    loadChildren: () => import('./verification/verification.module').then( m => m.VerificationPageModule)
   }
 
 ];
